d4f3e854ae74495cda782fd7719a7566f1e0bf95
Richard Black <rallenblack@gmail.com>
2017-06-08 15:43:01 -0400

    Updated codes to use scan table, source table

--- senstivity_maps/sensitivity_map.m
+++ senstivity_maps/sensitivity_map.m
@@ -4,10 +4,11 @@ clearvars;
 
 addpath ../kernel/
 scan_table; % Found in kernel directory
+source_table; % Found in kernel directory
 
 tic;
 
-pol = 'X';
+pol = 'Y';
 
 % % AGBT16B_400_01 Grid %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % session = AGBT16B_400_01;
@@ -15,17 +16,20 @@ pol = 'X';
 %             50, 65:70, 72, 74:78, 80:85];
 % off_scans = [33, 36, 39, 42, 45, 48, 51, 64, ... 
 %              71, 79];
+% source = source3C295;
 
 % AGBT16B_400_02 Grid %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% session = AGBT16B_400_02;
-% on_scans = [13:15, 17:19, 21:23, 25:27, 29:31,...
-%             33:35, 37:39, 41:43, 45:47, 49:51, 53:55];
-% off_scans = [16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56];
+session = AGBT16B_400_02;
+on_scans = [13:15, 17:19, 21:23, 25:27, 29:31,...
+            33:35, 37:39, 41:43, 45:47, 49:51, 53:55];
+off_scans = [16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56];
+source = source3C295;
 
 % % AGBT16B_400_03 Grid %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-session = AGBT16B_400_03;
-on_scans = [11:14, 16:18, 20:22, 24:26, 28:30, 32:34, 36:38, 40:42, 44:46, 48:50, 52:54];
-off_scans = [15, 19, 23, 27, 31, 35, 39, 43, 47, 51];
+% session = AGBT16B_400_03;
+% on_scans = [12:14, 16:18, 20:22, 24:26, 28:30, 32:34, 36:38, 40:42, 44:46, 48:50, 52:54];
+% off_scans = [15, 19, 23, 27, 31, 35, 39, 43, 47, 51];
+% source = source3C295;
 
 % % HI Source M101 - CALCORR
 % on_scans = 56;
@@ -63,7 +67,7 @@ out_dir = sprintf('%s/mat', save_dir);
 mkdir(save_dir, out_dir);
 
 % Constants
-overwrite = 0;
+overwrite = 1;
 k = 0;
 kB = 1.38*1e-23;
 
@@ -72,12 +76,13 @@ Ap = (rad^2)*pi;
 
 LO_freq = 1450;
 freqs = ((-249:250)*303.75e-3) + LO_freq;
-a = 1.4701;
-b = -0.7658;
-c = -0.2780;
-d = -0.0347;
-e = 0.0399;
-x = a + b*log10(freqs./1e3) + c*log10(freqs./1e3).^2 + d*log10(freqs./1e3).^3 + e*log10(freqs./1e3).^4;
+a = source.a;
+b = source.b;
+c = source.c;
+d = source.d;
+e = source.e;
+f = source.f;
+x = a + b*log10(freqs./1e3) + c*log10(freqs./1e3).^2 + d*log10(freqs./1e3).^3 + e*log10(freqs./1e3).^4 + f*log10(freqs./1e3).^5;
 flux_density = 10.^x;
 
 ant_dir = sprintf('%s/%s/Antenna', meta_root, proj_str);
@@ -131,7 +136,7 @@ for i = 1:length(on_tstamp)
     
     % Extract data and save
     if ~exist(filename, 'file') || overwrite == 1
-        [R, az, el, ~] = aggregate_banks(save_dir, ant_dir, tmp_stmp, 10);
+        [R, az, el, info] = aggregate_banks(save_dir, ant_dir, tmp_stmp, 10);
         save(filename, 'R', 'az', 'el');
     else
         load(filename);
@@ -169,31 +174,32 @@ for i = 1:length(on_tstamp)
     end
     
     fprintf('     Calculating weights and sensitivity...\n');
+    w = zeros(size(a));
     for t = 1:size(R,4)
         for b = 1:size(R,3)
             if sum(bad_freqs == b) == 0
-                % [a, ~] = eigs(R(good_idx, good_idx, b, t), OFF.R(good_idx, good_idx, b), 1);
-                w = OFF.R(good_idx, good_idx, b)\a(:,t,b);
-                w = w./(w'*a(:,t,b));
-                Pon = w'*R(good_idx, good_idx, b, t)*w;
-                Poff = w'*OFF.R(good_idx, good_idx, b)*w;
+                w(:,t,b) = OFF.R(good_idx, good_idx, b)\a(:,t,b);
+                w(:,t,b) = w(:,t,b)./(w(:,t,b)'*a(:,t,b));
+                Pon = w(:,t,b)'*R(good_idx, good_idx, b, t)*w(:,t,b);
+                Poff = w(:,t,b)'*OFF.R(good_idx, good_idx, b)*w(:,t,b);
                 SNR(t,b) = (Pon - Poff)/Poff;
                 Senstmp(t,b) = 2*kB*SNR(t,b)./(flux_density(b)*1e-26);
             else
                 Senstmp(t,b) = 0;
-                w = zeros(size(a,1),1);
-            end
-            if i == 1 && t == 1 && b == 1
-                w_agg = w;
-            else
-                % Append to aggregated steering vector matrix
-                w_agg = cat(2, w_agg, w);
+                w(:,t,b) = zeros(size(a,1),1);
             end
         end
         AZ = [AZ; az(t)];
         EL = [EL; el(t)];
     end
     Sens = [Sens; Senstmp];
+    
+    if i == 1
+        w_agg = w;
+    else
+        % Append to aggregated steering vector matrix
+        w_agg = cat(2, w_agg, w);
+    end
 end
 
 a_filename = sprintf('%s/%s_aggregated_grid_%s.mat', out_dir, session.session_name, pol);
@@ -208,10 +214,14 @@ save(w_filename, 'AZ', 'EL', 'w_agg');
 
 % Interpolated map
 Npoints = 100;
-minX = -0.8;
-maxX =  0.8;
-minY = -0.3;
-maxY =  0.3;
+% minX = -0.8;
+% maxX =  0.8;
+% minY = -0.3;
+% maxY =  0.3;
+minX = session.Xdims(1);
+maxX = session.Xdims(2);
+minY = session.Ydims(1);
+maxY = session.Ydims(2);
 xval = linspace(minX, maxX, Npoints);
 yval = linspace(minY, maxY, Npoints);
 [X,Y] = meshgrid(linspace(minX,maxX,Npoints), linspace(minY,maxY,Npoints));
@@ -224,26 +234,35 @@ for b = 101:101 %Nbins
     colorbar;
     set(gca, 'ydir', 'normal');
     colormap('jet');
-    xlabel('Azimuth Offset (degrees)');
+    xlabel('Cross-Elevation Offset (degrees)');
     ylabel('Elevation Offset (degrees)');
-    title('Formed Beam Sensitivity Map');
+    title(sprintf('Formed Beam Sensitivity Map - %spol', pol));
 end
 
-Tsys = Ap/max(max(Sens));
-
-% Tsys_eta = Ap./Sens;
+% Save figure
+fig_filename = sprintf('%s_%spol_map', session.session_name, pol);
+saveas(map_fig, sprintf('%s.png', fig_filename));
+saveas(map_fig, sprintf('%s.pdf', fig_filename));
+saveas(map_fig, sprintf('%s.eps', fig_filename));
+saveas(map_fig, sprintf('%s.fig', fig_filename), 'fig');
 
 % Get the angle of arrival for max sensitivity beam
 [s_max,max_idx] = max(Sens(:,101));
 Tsys_eta = Ap./(Sens(max_idx,:)*22.4./flux_density); % Remember to change when running again.
 
-
-figure(4);
+tsys_fig = figure();
 plot(freqs,real(Tsys_eta).');
 title('T_s_y_s/\eta_a_p vs Frequency');
 xlabel('Frequency (MHz)');
 ylabel('T_s_y_s/\eta_a_p (K)');
 grid on;
+
+% Save figure
+tsys_filename = sprintf('%s_%spol_tsys', session.session_name, pol);
+saveas(tsys_fig, sprintf('%s.png', tsys_filename));
+saveas(tsys_fig, sprintf('%s.pdf', tsys_filename));
+saveas(tsys_fig, sprintf('%s.eps', tsys_filename));
+saveas(tsys_fig, sprintf('%s.fig', tsys_filename), 'fig');
 
 toc;
 
