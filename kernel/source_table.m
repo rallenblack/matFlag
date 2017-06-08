9aee28fd4eb5e970a2d562bdbb75ccef0b5d172a
Richard Black <rallenblack@gmail.com>
2017-06-08 15:42:08 -0400

    Added source table script to keep track of source-dependent parameters

---
+++ kernel/source_table.m
@@ -0,0 +1,24 @@
+
+source3C295.name = '3C295';
+source3C295.a = 1.4701;
+source3C295.b = -0.7658;
+source3C295.c = -0.278;
+source3C295.d = -0.0347;
+source3C295.e = 0.0399;
+source3C295.f = 0;
+
+source3C48.name = '3C48';
+source3C48.a = 1.3253;
+source3C48.b = -0.7553;
+source3C48.c = -0.1914;
+source3C48.d = -0.0498;
+source3C48.e = 0;
+source3C48.f = 0;
+
+source3C147.name = '3C147';
+source3C147.a = 1.4516;
+source3C147.b = -0.6961;
+source3C147.c = -0.201;
+source3C147.d = -0.064;
+source3C147.e = -0.046;
+source3C147.f = 0.029;
