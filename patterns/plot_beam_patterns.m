bcece5ba5d7b0ffffa19b63c1265915d209677af
Richard Black <rallenblack@gmail.com>
2017-06-16 17:58:12 -0400

    Moved weight extraction code to dedicated function

--- patterns/plot_beam_patterns.m
+++ patterns/plot_beam_patterns.m
@@ -13,32 +13,10 @@ scan_table; % Found in kernel directory
 session = AGBT16B_400_02;
 pol = 'Y';
 
-% Beam locations
+% Get beam weights
 beam_el = [52.23,  52.32, 52.1,  52.19, 52.28, 52.07, 52.19];
 beam_az = [212.7,  212.9, 212.7, 212.9, 213.1, 212.9, 213.1];
-Nbeam = length(beam_el);
-
-% Get beam weights
-% Load in weights
-out_dir = sprintf('%s/%s/BF/mat', data_root, session.session_name);
-w_filename = sprintf('%s/%s_aggregated_weights_%s.mat', out_dir,...
-    session.session_name, pol);
-if ~exist(w_filename, 'file')
-    error(sprintf('The weight vectors file %s does not exist!\n',...
-        w_filename));
-end
-load(w_filename);
-
-% Find closest AZ/EL for specified beam locations
-for beam = 1:Nbeam
-    delta_el = (EL - beam_el(beam)).^2;
-    delta_az = (AZ - beam_az(beam)).^2;
-    d = sqrt(delta_el + delta_az);
-
-    [~, beam_idx(beam)] = min(d);
-end
-
-w = w_agg(:,beam_idx,:);
+w = get_grid_weights(session, pol, beam_az, beam_el);
 
 % Generate patterns
 [AZ, EL, patterns] = get_beamformed_patterns(session, pol, w);
