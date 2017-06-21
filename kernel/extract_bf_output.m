8e0c86efbc421dbb6c54d277465954d2023426f8
Mark Ruzindana <mruzinda@flag3.gb.nrao.edu>
2017-06-21 13:15:06 -0400

    Changes made to extract_bf_out.m and RTBF_analysis.m

--- kernel/extract_bf_output.m
+++ kernel/extract_bf_output.m
@@ -3,8 +3,25 @@ function [ B, dmjd, xid ] = extract_bf_output( fits_filename )
 %reconstructs it from the FITS format into a 3D matrix format
 
     info   = fitsinfo(fits_filename);
-    bintbl = fitsread(fits_filename, 'binarytable', 1);
-    
+%     Nrows = info.BinaryTable.Rows;
+    Nrows = 14000;
+    if info.FileSize < 4e9
+        bintbl = fitsread(fits_filename, 'binarytable', 1);
+    else
+        for idx = 1:4
+            tmpBintbl = fitsread(fits_filename, 'binarytable', ...
+                'Info', info, ...
+                'TableRows', [Nrows*(idx-1)+1:Nrows*idx]);
+            bintbl{1}(Nrows*(idx-1)+1:Nrows*idx, 1) = tmpBintbl{1};
+            bintbl{2}(Nrows*(idx-1)+1:Nrows*idx, 1) = tmpBintbl{2};
+            if length(tmpBintbl) == 3
+                bintbl{3}(Nrows*(idx-1)+1:Nrows*idx, :) = tmpBintbl{3};
+            else
+                bintbl{3}(Nrows*(idx-1)+1:Nrows*idx, 1) = tmpBintbl{3};
+                bintbl{4}(Nrows*(idx-1)+1:Nrows*idx, :) = tmpBintbl{4};
+            end
+        end
+    end
     dmjd = bintbl{1};
     mcnt = bintbl{2};
     if length(bintbl) == 3
@@ -26,9 +43,10 @@ function [ B, dmjd, xid ] = extract_bf_output( fits_filename )
     Npol = 4; % X self-polarized, Y self-polarized, XY polarized (real), XY polarized (imaginary)
     Nbeam = 7;
     Nbin = 25;
+    Nblocks = size(data,1);
     Nsti = 100;
 
-    B = reshape(data, Nbeam, Npol, Nbin, Nsti);
+    B = reshape(data, Nblocks, Nbeam, Npol, Nbin, Nsti);
     
 end
 
--- patterns/RTBF_data_analysis.m
+++ patterns/RTBF_data_analysis.m
@@ -20,20 +20,39 @@ save_dir = sprintf('%s/%s/BF', data_root, proj_str);
 out_dir = sprintf('%s/mat', save_dir);
 % mkdir(save_dir, out_dir);
 
-banks = 'A';
+banks = 'A'; % F is a small enough bank since it stalled during observation
 
-filename = sprintf('%s/%s%s.fits', save_dir, on_tstamp{2}, banks);
+filename = sprintf('%s/%s%s.fits', save_dir, on_tstamp{1}, banks);
 
 b = extract_bf_output(filename);
 
-N_bin = size(b,3); % 25
-N_pol = size(b,2); % 4  % Order: XX*, YY*, real(XY*), and imag(XY*).
+N_sti = size(b,5); % 100
+N_bin = size(b,4); % 25
+N_pol = size(b,3); % 4  % Order: XX*, YY*, real(XY*), and imag(XY*).
+N_beam = size(b,2); % 7
+N_block = size(b,1);
 idx = 0;
-for i = 1:1 % N_bin
+
+beam = zeros(N_beam ,N_pol, N_bin, N_block*N_sti);
+for nb = 1:N_block
+    beam(:,:,:,(N_sti*(nb-1)+1):(N_sti*nb)) = b(nb,:,:,:,:);
+end
+
+% L = size(beam,4);
+% Fs = 
+for i = 11:11 % N_bin
     for k = 1:1 % Order: XX*, YY*, real(XY*), and imag(XY*).
+        for nbeam = 1:N_beam
+            Y1 = fft(fftshift(beam(nbeam,k,i,:)));
+        end
+        P1 = abs(Y1(:,:,:,:));
+        L = size(P1,4);
+%         fq = Fs*(0:(L/2))/L;
+        fq = 0:(7.59e3)/(L-1):7.59e3;
         idx = idx+1;
         figure(idx);
-        imagesc(squeeze(10*log10(abs(b(:,k,i,:)))).'); % Order: beam, pol, bin, and sti.
+        plot(fq,squeeze(P1).'); % Order: block, beam, pol, bin, and sti.
+        grid on;
         if k == 1
             title(['X Frequency bin ' num2str(i)]);
         elseif k == 2
@@ -43,8 +62,31 @@ for i = 1:1 % N_bin
         elseif k == 4
             title(['Imaginary XY Frequency bin ' num2str(i)]);
         end
-        xlabel('Beam Index');
-        ylabel('STI Index');
+        ylabel('Power');
+        xlabel('Frequncy (Hz)');
+    end
+end
+
+for i = 1:1 % N_beam
+    for k = 1:1 % Order: XX*, YY*, real(XY*), and imag(XY*).
+        idx = idx+1;
+        figure(idx);
+        plot(squeeze(abs(beam(i,k,:,:))).'); % Order: beam, pol, bin, and sti.
+        grid on;
+        title(['Beam ' num2str(i)]);
+        ylabel('Power');
+        xlabel('STI Index');
+    end
+end
+
+for i = 1:1 % N_beam
+    for k = 1:1 % Order: XX*, YY*, real(XY*), and imag(XY*).
+        idx = idx+1;
+        figure(idx);
+        imagesc(squeeze(10*log10(abs(beam(i,k,:,:))))); % Order: beam, pol, bin, and sti.
+        title(['Beam ' num2str(i)]);
+        ylabel('Frequency Bin Index');
+        xlabel('STI Index');
     end
 end
 
