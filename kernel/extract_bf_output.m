ad8e775e22b37a758a3141b941ecefccb8715a8c
Richard Black <rallenblack@gmail.com>
2017-08-31 15:27:52 -0400

    Updated beamformer interpretation code to examine a subset of time samples

--- kernel/extract_bf_output.m
+++ kernel/extract_bf_output.m
@@ -1,53 +1,62 @@
-function [ B, dmjd, xid ] = extract_bf_output( fits_filename )
+function [ B, xid ] = extract_bf_output( fits_filename )
 %EXTRACT_BEAMFORMER_OUTPUT Function that extracts the data from the RTBF and
 %reconstructs it from the FITS format into a 3D matrix format
-
-    info   = fitsinfo(fits_filename);
-%     Nrows = info.BinaryTable.Rows;
-    Nrows = 14000;
-    if info.FileSize < 4e9
-        bintbl = fitsread(fits_filename, 'binarytable', 1);
+    import matlab.io.*
+    % Open file
+    fptr = fits.openFile(fits_filename);
+    
+    % Get xid
+    [xid,~] = fits.readKeyDbl(fptr, 'XID');
+    
+    % Move the pointer to read binary table
+    fits.movAbsHDU(fptr,2);
+    
+    % Get the number of rows
+    totalRows = fits.getNumRows(fptr);
+    
+    % Read the 4th column which contains the data
+    colidx = 4;
+    stepSize = 1000; % 10000;
+    if stepSize > totalRows
+        stepSize = totalRows;
+        fluxdata = fits.readCol(fptr,colidx,1,stepSize);
     else
-        for idx = 1:4
-            tmpBintbl = fitsread(fits_filename, 'binarytable', ...
-                'Info', info, ...
-                'TableRows', [Nrows*(idx-1)+1:Nrows*idx]);
-            bintbl{1}(Nrows*(idx-1)+1:Nrows*idx, 1) = tmpBintbl{1};
-            bintbl{2}(Nrows*(idx-1)+1:Nrows*idx, 1) = tmpBintbl{2};
-            if length(tmpBintbl) == 3
-                bintbl{3}(Nrows*(idx-1)+1:Nrows*idx, :) = tmpBintbl{3};
-            else
-                bintbl{3}(Nrows*(idx-1)+1:Nrows*idx, 1) = tmpBintbl{3};
-                bintbl{4}(Nrows*(idx-1)+1:Nrows*idx, :) = tmpBintbl{4};
-            end
-        end
-    end
-    dmjd = bintbl{1};
-    mcnt = bintbl{2};
-    if length(bintbl) == 3
-        data = bintbl{3};
-    else
-        good_data = bintbl{3};
-        data = bintbl{4};
-    end
-
-    keywords = info.PrimaryData.Keywords;
-    xid = -1;
-    for i = 1:size(keywords, 1)
-        if strcmp(keywords{i,1}, 'XID')
-            xid = str2double(keywords{i,2});
-            break;
-        end
+        fluxdata = fits.readCol(fptr,colidx,7000,stepSize);
     end
     
+    
+%     % Get the size of a row
+%     fluxdata = fits.readCol(bintbl,colidx,1,1);
+%     repeat = size(fluxdata,2);
+%     % Preallocate the array
+%     fluxdata = zeros(totalRows, repeat, 'like', fluxdata);
+%     % Read all row in a loop
+%     for i = 1:stepSize:totalRows
+%         display(['Row : ', i]);
+%         if ((i+stepSize) > totalRows)
+%             index = totalRows;
+%             numRows = totalRows - i + 1;
+%         else
+%             index = i + stepSize - 1;
+%             numRows = stepSize;
+%         end
+%         fluxdata(i:index,:) = fits.readCol(bintbl,colidx,i,numRows);
+%     end
+
+    % Close file
+    fits.closeFile(fptr);
+
+    Nrows = size(fluxdata, 1);   
     Npol = 4; % X self-polarized, Y self-polarized, XY polarized (real), XY polarized (imaginary)
     Nbeam = 7;
     Nbin = 25;
-    Nblocks = size(data,1);
     Nsti = 100;
-
-    B = reshape(data, Nblocks, Nbeam, Npol, Nbin, Nsti);
     
+%     B = reshape(data, Nrows, Nbeam, Npol, Nbin, Nsti);
+    B = zeros(Nbeam, Npol, Nbin, Nsti*Nrows);
+    for r = 1:Nrows
+        B(:,:,:,Nsti*(r-1)+1:Nsti*r) = reshape(fluxdata(r,:), Nbeam, Npol, Nbin, Nsti);
+    end
 end
 
 
